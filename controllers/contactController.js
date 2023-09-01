4cdf8426c839116a30f58ac60c5c64fcf6d78cdd
aura-gv <gauravkhatiwada4@gmail.com>
2023-09-01 19:20:35 +0545

    mongoDb connection

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,29 +1,36 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc get all contacts
 //@route GET /api/contacts
 //@access Public
 
-const getContacts = async (req, res) => {
-  res.status(200).json({ message: "Get Contact list" });
-};
-const getContactsI = async (req, res) => {
-  res.json({ message: `Get Contact list for ${req.params.id}` });
-};
+// to resolve promise use async
+// async handler handles try catch so we dont have to use try catch
 
-const createContact = async (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get Contact list" });
+});
+
+const getContactsI = asyncHandler(async (req, res) => {
+  res.json({ message: `Get Contact list for ${req.params.id}` });
+});
+
+const createContact = asyncHandler(async (req, res) => {
   console.log(req.body);
   const { name, email } = req.body;
   if (!name || !email) {
     res.status(400).json({ message: "Please provide name and email" });
   }
   res.status(200).json({ message: "Create Contact list" });
-};
+});
 
-const deleteContact = async (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Delete Contact list" });
-};
-const updateContact = async (req, res) => {
+});
+
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Update Contact list" });
-};
+});
 
 module.exports = {
   getContacts,
